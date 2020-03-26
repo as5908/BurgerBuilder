9face245bb5a717ed17bddaf6edfffbfa93cf932
twmrdx <abhinavsahai01+sourcetree@gmail.com>
2020-03-26 15:25:48 -0700

    End of section 8

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,45 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(
-    props.ingredients
-  ).map(igKey => {
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log('Order summary will update');
+  }
+  render() {
+    const ingredientSummary = Object.keys(
+      this.props.ingredients
+    ).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>
+            {igKey}
+          </span>
+          :{this.props.ingredients[igKey]}
+        </li>
+      );
+    });
+
     return (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>
-          {igKey}
-        </span>
-        :{props.ingredients[igKey]}
-      </li>
+      <>
+        <h3>Your Order</h3>
+        <p>
+          A delicious burger with following ingredients:
+        </p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>
+            Total Price : {this.props.price.toFixed(2)}
+          </strong>
+        </p>
+        <p>Continue to checkout ?</p>
+        <Button
+          clicked={this.props.purchaseCancelled}
+          btnType='Danger'
+        >
+          CANCEL
+        </Button>
+        <Button
+          clicked={this.props.purchaseContinued}
+          btnType='Success'
+        >
+          CONTINUE
+        </Button>
+      </>
     );
-  });
+  }
+}
 
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>A delicious burger with following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>
-          Total Price : {props.price.toFixed(2)}
-        </strong>
-      </p>
-      <p>Continue to checkout ?</p>
-      <Button
-        clicked={props.purchaseCancelled}
-        btnType='Danger'
-      >
-        CANCEL
-      </Button>
-      <Button
-        clicked={props.purchaseContinued}
-        btnType='Success'
-      >
-        CONTINUE
-      </Button>
-    </>
-  );
-};
-
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-  <>
-    <Backdrop
-      show={props.show}
-      clicked={props.modalClosed}
-    />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show
-          ? 'translateY(0)'
-          : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}
-    >
-      {props.children}
-    </div>
-  </>
-);
+// class Modal extends Component {
+//   shouldComponentUpdate(nextProps, nextState) {
+//     return nextProps.show !== this.props.show;
+//   }
 
-export default modal;
+//   componentWillUpdate() {
+//     console.log('Component will update');
+//   }
+//   render() {
+//     return (
+//       <>
+//         <Backdrop
+//           show={this.props.show}
+//           clicked={this.props.modalClosed}
+//         />
+//         <div
+//           className={classes.Modal}
+//           style={{
+//             transform: this.props.show
+//               ? 'translateY(0)'
+//               : 'translateY(-100vh)',
+//             opacity: this.props.show ? '1' : '0'
+//           }}
+//         >
+//           {this.props.children}
+//         </div>
+//       </>
+//     );
+//   }
+// }
+// export default Modal;
+
+const memoDependencyFunction = (prevProps, nextProps) =>
+  prevProps.show === nextProps.show;
+
+const Modal = props => {
+  console.log(props);
+  return (
+    <>
+      <Backdrop
+        show={props.show}
+        clicked={props.modalClosed}
+      />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show
+            ? 'translateY(0)'
+            : 'translateY(-100vh)',
+          opacity: props.show ? '1' : '0'
+        }}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+};
+
+export default React.memo(Modal, memoDependencyFunction);
